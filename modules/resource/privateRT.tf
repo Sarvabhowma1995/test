aa88da76c56d69f084866f20c2d4097cbf15828d
root <root@ip-172-31-6-90.ap-south-1.compute.internal>
2021-08-05 04:25:14 +0000

    added the files

--- modules/resource/privateRT.tf
+++ modules/resource/privateRT.tf
@@ -1,9 +1,6 @@
 resource "aws_route_table" "privateRT" {
     vpc_id = aws_vpc.firstvpc.id
-    route {
-        cidr_block = var.privatesubnetid
-        gateway_id = aws_internet_gateway.gw.id
-    }
+    route = []
 
 	tags = {
 		Name = var.privateRTname
--- modules/resource/publicRT.tf
+++ modules/resource/publicRT.tf
@@ -1,7 +1,7 @@
 resource "aws_route_table" "publicRT" {
     vpc_id = aws_vpc.firstvpc.id
     route {
-        cidr_block = var.publicsubnetid
+        cidr_block = "0.0.0.0/0"
         gateway_id = aws_internet_gateway.gw.id
     }
 
